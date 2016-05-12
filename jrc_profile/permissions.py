49827482c4da2bbf4bf67a1e2b616c8bef489c18
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-05-12 11:01:37 +0200

    Fallback to IsAdminUser when browsing API in web browser

--- jrc_profile/permissions.py
+++ jrc_profile/permissions.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import BasePermission
 
 AUTH_API = 'https://auth.jrc.no/'
 
+
 def check_token(token):
     r = requests.post(
         AUTH_API + 'check-token/',
@@ -16,7 +17,11 @@ def check_token(token):
 class HasValidJrcAuthToken(BasePermission):
     '''
     Only allow access if the auth-token HTTP header is valid.
+    Fall back to IsAdminUser when browsing the API.
     '''
     def has_permission(self, request, view):
-        assert 'HTTP_AUTH_TOKEN' in request.META.keys(), "Http-Auth-Token header not set"
-        return check_token(request.META['HTTP_AUTH_TOKEN'])
+        if 'accept/json' in request.META['HTTP_ACCEPT']:
+            assert 'HTTP_AUTH_TOKEN' in request.META.keys(), \
+                "Http-Auth-Token header not set"
+            return check_token(request.META['HTTP_AUTH_TOKEN'])
+        return request.user and request.user.is_staff
