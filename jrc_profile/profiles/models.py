52fad1182828899bc24d97e610375066de3fb00e
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-05-20 15:45:55 +0200

    Add image and title fields, add suport for profile creation

--- jrc_profile/profiles/models.py
+++ jrc_profile/profiles/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 
 class Profile(models.Model):
+    CONSULTANT = 'CONSULTANT'
+    PARTNER = 'PARTNER'
+    TITLE_CHOICES = (
+        (CONSULTANT, 'Consultant'),
+        (PARTNER, 'Partner')
+    )
     auth_id = models.BigIntegerField(primary_key=True)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    phone_nr = models.CharField(max_length=40)
-    bio_text = models.TextField()
-    fun_fact = models.TextField()
-    active = models.BooleanField()
+    phone_nr = models.CharField(max_length=40, blank=True)
+    bio_text = models.TextField(blank=True)
+    fun_fact = models.TextField(blank=True)
+    active = models.BooleanField(blank=True)
+    title = models.CharField(max_length=40, choices=TITLE_CHOICES, blank=True)
+    image = models.ImageField(upload_to='profile_pictures', blank=True)
--- jrc_profile/profiles/serializers.py
+++ jrc_profile/profiles/serializers.py
@@ -3,6 +3,6 @@ from rest_framework import serializers
 from models import Profile
 
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
