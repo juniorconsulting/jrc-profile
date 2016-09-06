375e3f52c5101c967dcf273091b05cb8f98662f5
Ole Andreas Hansen <oleaha@me.com>
2016-09-06 20:03:11 +0200

    Make it possible to change profiles from /admin

--- jrc_profile/profiles/models.py
+++ jrc_profile/profiles/models.py
@@ -17,3 +17,6 @@ class Profile(models.Model):
     active = models.BooleanField(blank=True, default=False)
     title = models.CharField(max_length=40, choices=TITLE_CHOICES, blank=True, null=True)
     image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
--- jrc_profile/urls.py
+++ jrc_profile/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
 
 from profiles import views
 
@@ -10,6 +11,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^profiles/$', views.profile_list),
+    url(r'^admin/', admin.site.urls),
     url(r'^profiles/(?P<pk>[0-9]+)/$', views.profile_detail),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
